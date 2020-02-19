7ecc7255cc186ab3adf3ad662b233fdac3341136
Andrey <56323140+AndreyNazarov@users.noreply.github.com>
2020-02-19 17:03:51 +0100

    Add files via upload

--- js2/task6.js
+++ js2/task6.js
@@ -1,16 +1,16 @@
 let input = document.querySelector('input[name="number"]');
-const btnMore = document.querySelector("#more");
-const btnTotal = document.querySelector("#total");
+const btnMore = document.querySelector('#more');
+const btnTotal = document.querySelector('#total');
 const numbers = [];
 let total = 0;
 
-btnMore.addEventListener("click", function() {
+btnMore.addEventListener('click', function() {
   const value = Number(input.value);
   numbers.push(value);
-  input.value = "";
+  input.value = '';
 });
 
-btnTotal.addEventListener("click", function() {
+btnTotal.addEventListener('click', function() {
   for (const number of numbers) {
     total += number;
   }
